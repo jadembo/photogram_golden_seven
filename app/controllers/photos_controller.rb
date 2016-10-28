ee06e77c8c0da354c4a10d149574d741ac4cca78
Justin A. Dembo <jdembo@chicagobooth.edu>
2016-10-28 12:26:04 -0500

    Enabled View

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,18 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @list_of_photos = Photo.all
+
+    @photo = @list_of_photos.find(params["id"])
+
+    render ("show_photo.html.erb")
+  end
+
+  def edit
+
+
+    render("edit_photo.html.erb")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/:id/edit"   { :controller => "photos", :action => "edit" })
 
 end
