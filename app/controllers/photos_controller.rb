c3e81eb297e58722e171d702fae1c52428679635
Justin A. Dembo <jdembo@chicagobooth.edu>
2016-10-29 08:15:21 -0500

    Enable Create & Delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,12 @@
 class PhotosController < ApplicationController
   def index
+    if params["photo_caption"].nil? == false && params["image_url"].nil? == false
+      g = Photo.new
+      g.source = params["image_url"]
+      g.caption = params["photo_caption"]
+      g.save
+    end
+
     @list_of_photos = Photo.all
   end
 
@@ -20,7 +27,6 @@ class PhotosController < ApplicationController
 
     @photo = @list_of_photos.find(params["id"])
 
-
     render ("show_photo.html.erb")
   end
 
@@ -32,6 +38,24 @@ class PhotosController < ApplicationController
 
 
     render("edit_photo.html.erb")
+  end
+
+  def delete
+    @list_of_photos = Photo.all
+
+    @photo = @list_of_photos.find(params["id"])
+    @photo.destroy
+
+    @list_of_photos = Photo.all
+
+    render("index.html.erb")
+
+  end
+
+  def create
+
+    render ("create.html.erb")
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/new",           { :controller => "photos", :action => "create" })
   get("/photos/:id/edit",   { :controller => "photos", :action => "edit" })
+  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/delete_photo/:id",       { :controller => "photos", :action => "delete" })
+
+
+
 
 end
